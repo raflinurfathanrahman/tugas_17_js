4ee74bf5c590730e538d729dfb3ba1eca42bd851
raflirahman <raflirahman375@gmail.com>
2022-10-04 12:33:49 +0700

    add tugas 17

---
+++ tugas17.js
@@ -0,0 +1,6 @@
+var kata = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+console.log(kata.toLowerCase());
+console.log(kata.toUpperCase());
+var kata2 = "allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(kata2.toLowerCase());
+console.log(kata2.toUpperCase());
